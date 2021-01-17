d469bf64c2b67cb8aeb8f2576bb527fbbfbaf329
Iyman Ahmed <iymanahmed8@gmail.com>
2021-01-17 16:19:51 +0500

    Add files via upload

---
+++ Random Forest Regression/Random_Forest_Regression_Model.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:-1]
+y = dataset.iloc[:,-1]
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(X,y)
+
+prediction=regressor.predict([[6.5]])
+print(prediction)
+
+
+x_grid = np.arange(1, 10, 0.1)
+x_grid = x_grid.reshape(len(x_grid), 1)
+plt.scatter(X,y,color='blue')
+plt.plot(x_grid,regressor.predict(x_grid),color='red')
+plt.title('Position Vs Salary')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
